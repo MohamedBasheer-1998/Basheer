e733e59c0da6ba51c67a7ed9710cf7a431ab80b4
Starvin A <StarvinA@tritonlabs.in>
2022-06-20 13:37:31 +0530

    create

---
+++ Model/Models.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Basheer.Model
+{
+    public class Models
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
